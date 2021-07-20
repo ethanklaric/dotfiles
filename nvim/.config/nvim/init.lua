c9ceec9c02d603355d6a1e3dbd8fe8d046ad7d30
Ethan Klaric <ethanklaric02@gmail.com>
2021-07-20 16:42:23 +1000

    initial commit

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,20 @@
+-- load all plugins
+require "pluginList"
+require "options"
+
+local g = vim.g
+
+g.mapleader = " "
+g.auto_save = false
+
+-- colorscheme related stuff
+
+g.nvchad_theme = "wal"
+local base16 = require "base16"
+base16(base16.themes["onedark"], true)
+
+require "highlights"
+require "mappings"
+require "plugins.bufferline"
+
+require("utils").hideStuff()
