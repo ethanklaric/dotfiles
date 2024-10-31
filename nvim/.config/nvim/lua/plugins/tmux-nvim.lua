034ef53b42856673c551db1879d009d6eaef11e0
Ethan Klaric <s3920200@student.rmit.edu.au>
2024-10-31 23:26:40 +1100

    C-hjkl now transitions between nvim+tmux panes

---
+++ nvim/.config/nvim/lua/plugins/tmux-nvim.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "aserowy/tmux.nvim",
+    config = function()
+      return require("tmux").setup()
+    end,
+  },
+
+  {
+    "alexghergh/nvim-tmux-navigation",
+    event = "VeryLazy",
+    config = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
+      nvim_tmux_nav.setup({
+        disable_when_zoomed = true,
+        -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        },
+      })
+    end,
+  },
+}
