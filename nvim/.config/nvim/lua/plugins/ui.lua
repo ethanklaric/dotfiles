d675e2e72ff74aee042fe6b7f4ddc2b066a480df
Ethan Klaric <s3920200@student.rmit.edu.au>
2024-10-31 23:36:12 +1100

    ui updates - smaller notif, no command popout

---
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+
+  {
+    "j-hui/fidget.nvim",
+    opts = {
+      notification = {
+        window = {
+          winblend = 0,
+          border = "rounded",
+        },
+      },
+    },
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 500,
+      render = "compact",
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.25)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+    },
+  },
+
+  -- bufferline
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+}
